d881dab9ac0d2ac85da4af437f1c4bf4a637db4f
Debora Santos Silva. <ds4134092@gmail.com>
2025-09-24 08:33:47 -0300

    Update loja_de_info.py

--- loja_de_info.py
+++ loja_de_info.py
@@ -12,16 +12,6 @@ from datetime import datetime
  
 @dataclass
 
-def menu():
-    print("\n--- COMPUTARIA supports.INFO ---")
-    print("1 - Cadastro Cliente")
-    print("2 - Cadastro de serviço")
-    print("3 - Ordem de serviço")
-    print("4 - Controle de estoque")
-    print("5 - Fale conosco")
-    print("6 - Relatório")
-    print("0 - Sair")
-    return input("Escolha uma opção: ")
 
 class Cadastro_cliente:
      nome:str
@@ -38,63 +28,23 @@ class Cadastro_de_Servico:
      remocao_de_virus:str
      instalacao_de_programas:str
 
- def Cadastro_de_Os():
+def Cadastro_de_Os():
     nome_cliente = input("Qual o nome do cliente")
     problema = input("Qual o problema dele")
     cadastro_de_Servico  = Cadastro_de_Servico (formatacao,limpeza_geral,troca_de_pecas,remocao_de_virus,instalacao_de_programas)
     print("Qual o problema da sua máquina?")
+
+def menu():
+    print("\n--- COMPUTARIA supports.INFO ---")
+    print("1 - Cadastro Cliente")
+    print("2 - Cadastro de serviço")
+    print("3 - Ordem de serviço")
+    print("4 - Controle de estoque")
+    print("5 - Fale conosco")
+    print("6 - Relatório")
+    print("0 - Sair")
+    return input("Escolha uma opção: ")
     
 while True:
-    
-     opcao = menu()
-
-    if opcao == "1":
-        nome = input("Digite o nome do cliente: ")
-        numero = input("Digite o número do cliente: "))
-        print(f" Cliente cadastrado com sucesso!")
-
-    elif opcao == "2":
-        if nome is None:
-            print("Nenhum cadastro de serviço feito.")
-        else:
-            retirar = int(input("Digite a quantidade de produtos que você deseja "))
-            if retirar <= 0:
-                print("A quantidade deve ser maior que zero!")
-            elif retirar > quantidade:
-                print("Quantidade insuficiente no estoque!")
-            else:
-                quantidade -= retirar
-                print(f"Retirado {retirar} unidade(s). Estoque atual: {quantidade}")
-
-    elif opcao == "3":
-        if nome is None:
-            print("Nenhum evento cadastrado ainda!")
-        else:
-            adicionar = int(input("Digite a quantidade a adicionar: "))
-            if adicionar <= 0:
-                print("A quantidade deve ser maior que zero!")
-            else:
-                quantidade += adicionar
-                print(f"Adicionado {adicionar} ingresso(s). Estoque atual: {quantidade}")
-
-    elif opcao == "4":
-        if nome is None:
-            print("Nenhum evento cadastrado ainda!")
-        else:
-            print(f"Evento {nome} | Quantidade em estoque: {quantidade}")
-
-    elif opcao == "0":
-        print("Saindo do sistema... até mais!")
-        break
-
-    else:
-        print("Opção inválida! Tente novamente.")
-    
-    
-    
-    
-    
-    
-    
     
     
